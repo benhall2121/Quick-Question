e71b330ac7524083dfae53aa911f9109d2863b03
Ben Hall <benhall2121@yahoo.com>
2011-08-11 23:48:12 -0700

    benhall2121@yahoo.com facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'ghWeXRr6E82mo9tRuPU9A', 'FEcThgUFHhrKiPRIAoy6a2khF4fyN8S9odTfFmMg'
-  provider :facebook, "131985646895644", "91c62cfef0a20bfaa04418310aade017"
+  provider :facebook, "131985646895644", "91c62cfef0a20bfaa04418310aade017", {:scope => 'publish_stream,offline_access,email'}
   #provider :open_id, OpenID::Store::Filesystem.new('/tmp')
 end
