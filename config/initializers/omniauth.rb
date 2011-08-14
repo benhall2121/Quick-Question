2316c6c24832059e76702ed5fdc71a47c9baf5d3
Ben Hall <benhall2121@yahoo.com>
2011-08-14 01:49:39 -0700

    benhall2121@yahoo.com facebook might be posting now

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'ghWeXRr6E82mo9tRuPU9A', 'FEcThgUFHhrKiPRIAoy6a2khF4fyN8S9odTfFmMg'
   #provider :facebook, "131985646895644", "91c62cfef0a20bfaa04418310aade017", {:scope => 'publish_stream,offline_access,email'}
-  provider :facebook, "131985646895644", "91c62cfef0a20bfaa04418310aade017", {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+  provider :facebook, "131985646895644", "91c62cfef0a20bfaa04418310aade017", {:scope => 'publish_stream,offline_access,email', :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
   #provider :open_id, OpenID::Store::Filesystem.new('/tmp')
 end
